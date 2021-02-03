f12270b5e8d240e13be601d63912ba639ff1d9e7
Cameron Battagler <fictoris@gmail.com>
2021-02-03 16:11:52 -0600

    Initial Commit
    Added .gitignore to ignore output folders
    Added Primary Script
    Provided useful README

---
+++ AnalyzeAndRecommendSqlSku.ps1
@@ -0,0 +1,86 @@
+<#
+.SYNOPSIS
+    Quickly create recommendations for which SQL Azure Sku to run based on current SQL Server usage
+.DESCRIPTION
+    This script wraps the process of collecting and analyzing database performance counters to recommend a SQL Azure SKU into a simple script.
+    Please capture at least 40 minutes worth of data for a valid recommendation.
+    Prerequisites:
+    - Please make sure you are running on PowerShell Version 5.1
+    - Please install the latest version of Database Migration Assistant
+    - Please install the Azure PowerShell Module
+    - Please go to https://aka.ms/dmaskuprereq for further information
+
+.EXAMPLE
+    To analyze for 40 minutes and get a recommendation
+    .\AnalyzeAndRecommendSqlSku.ps1 -CollectionTimeInSeconds 2400 -ConnectionString "Server=myServerAddress;Initial Catalog=master;Trusted_Connection=True;"
+#>
+param
+(
+    [Parameter(Mandatory=$true)][int]$CollectionTimeInSeconds,
+    [Parameter(Mandatory=$true)][string]$ConnectionString
+)
+function ExtractServerNameFromConnectionStringForComputerName
+{
+    param
+    (
+        [Parameter(Mandatory=$true)][string]$DbConnectionString
+    )
+
+    $splitString = $DbConnectionString -split ';'
+
+    foreach ($token in $splitString)
+    {
+        if ($token.StartsWith("Server="))
+        {
+            if($token.Contains("\")){
+                $server = ($token -split "\",-1,'SimpleMatch')[0]
+            } else {
+                $server = $token
+            }
+            if($server.Contains(",")) {
+                $server = ($token -split ",")[0]
+            }
+            return $server.Replace("Server=", "").Trim()
+        }
+    }
+    return "";
+}
+
+$DMADirectory = "C:\Program Files\Microsoft Data Migration Assistant\"
+
+If($PSVersionTable.PSVersion.Major -gt 5) {
+    Write-Host "To collect machine information, the data collection script uses the Get-WmiObject cmdlet, which was deprecated in PowerShell 6. To run this script in PowerShell 6 or 7, you must replace the WMI cmdlets with the newer CIM cmdlets."
+    Write-Host "Please go to https://aka.ms/dmaskuprereq for a list of prequisites."
+    return
+}
+
+If(!(Test-Path -Path $DMADirectory -PathType Container)) {
+    Write-Host "Please install the newest version of the Database Migration Assistant."
+    Write-Host "Please go to https://aka.ms/dmaskuprereq for a list of prequisites."
+    return
+}
+
+if(!(Get-Module -Name Az.Accounts -ListAvailable)) {
+    Write-Host "Ensure that your computer has the Azure Powershell Module installed."
+    Write-Host "Please go to https://aka.ms/dmaskuprereq for a list of prequisites."
+    return
+}
+
+$ComputerName = ExtractServerNameFromConnectionStringForComputerName $ConnectionString
+
+New-Item -ItemType Directory -Force -Path "$PSScriptRoot\Counters\", "$PSScriptRoot\$ComputerName-RecommenderOutput\" | Out-Null
+
+$SkuRecommenderScript = $DMADirectory + "SkuRecommendationDataCollectionScript.ps1"
+
+& $SkuRecommenderScript -ComputerName $ComputerName -OutputFilePath "$PSScriptRoot\Counters\$ComputerName-counters.csv" -CollectionTimeInSeconds $CollectionTimeInSeconds -DbConnectionString $ConnectionString
+
+If(!(Test-Path -Path "$PSScriptRoot\Counters\$ComputerName-counters.csv" -PathType Leaf)) {
+    Write-Host "The SQL Server information capturing script failed to create an output, please see above for more troubleshooting information."
+    
+    return
+} else {
+    $DmaCmd = $DMADirectory + "DmaCmd.exe"
+    & $DmaCmd /Action=SkuRecommendation /SkuRecommendationInputDataFilePath="$PSScriptRoot\Counters\$ComputerName-counters.csv" /SkuRecommendationTsvOutputResultsFilePath="$PSScriptRoot\$ComputerName-RecommenderOutput\$ComputerName-prices.tsv" /SkuRecommendationJsonOutputResultsFilePath="$PSScriptRoot\$ComputerName-RecommenderOutput\$ComputerName-prices.json" /SkuRecommendationOutputResultsFilePath="$PSScriptRoot\$ComputerName-RecommenderOutput\$ComputerName-prices.html" /SkuRecommendationPreventPriceRefresh=true
+    Write-Host "Recommendations Complete!"
+    Write-Host "View your recommendations at $PSScriptRoot\$ComputerName-RecommenderOutput\"
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # AnalyzeAndRecommendSqlSku
+    
+Quickly create recommendations for which SQL Azure Sku to run based on current SQL Server usage
+
+## Description
+
 This script wraps the process of collecting and analyzing database performance counters to recommend a SQL Azure SKU into a simple script.
+Please capture at least 40 minutes worth of data for a valid recommendation.
+Prerequisites:
+- Please make sure you are running on PowerShell Version 5.1
+- Please install the latest version of [Database Migration Assistant](https://aka.ms/get-dma)
+- Please install the [Azure PowerShell Module](https://docs.microsoft.com/en-us/powershell/azure/install-az-ps?view=azps-1.8.0&preserve-view=true)
+- Please go to https://aka.ms/dmaskuprereq for further information
+
+## Example
+
+To analyze for 40 minutes and get a recommendation
+`.\AnalyzeAndRecommendSqlSku.ps1 -CollectionTimeInSeconds 2400 -ConnectionString "Server=myServerAddress;Initial Catalog=master;Trusted_Connection=True;"`
